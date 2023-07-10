dabd98fcf3d67f041271d685f0100c271bccfd8d
Yash Pandey <109035687+yashpandey474@users.noreply.github.com>
2023-07-10 14:53:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ import ReactDOM from "react-dom";
 //VARIABLE TO PUT IN HTML
 const fname = "Yash";
 const lName = "Pandey";
+const headingStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
+//CHANGE STYLE WITH THE OBJECT
+headingStyle.color = "blue";
+
 //RENDER USING REACT [BABEL HELPS TO CONVERT EMBEDDED HTML]
 ReactDOM.render(
   //SINGLE ELEMENT ESSENTIALLY
   <div>
-    <h1>
-      Hello {fname + " " + lName}!
-    </h1>
+    <h1 style={headingStyle}> Hello {fname + " " + lName}!</h1>
     <p> Your lucky number is {Math.floor(Math.random() * 10)} </p>
   </div>,
   document.getElementById("root")
