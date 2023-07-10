95d152ac673c71e759afc18e08527a382ed37950
Yash Pandey <109035687+yashpandey474@users.noreply.github.com>
2023-07-10 14:03:35 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+//VARIABLE TO PUT IN HTML
+const fname = "Yash";
+const lName = "Pandey";
 //RENDER USING REACT [BABEL HELPS TO CONVERT EMBEDDED HTML]
 ReactDOM.render(
   //SINGLE ELEMENT ESSENTIALLY
   <div>
-    <h1> Hello World! </h1>
-    <p> This is a paragraph </p>
+    <h1>
+      Hello {fname + " " + lName}!
+    </h1>
+    <p> Your lucky number is {Math.floor(Math.random() * 10)} </p>
   </div>,
   document.getElementById("root")
 );
